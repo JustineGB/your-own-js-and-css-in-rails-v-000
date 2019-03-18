d2888f62c6784e5c693995a0f2d8635b712b6450
Justine B <justinegayton@gmail.com>
2019-03-18 18:44:45 +0000

    Automatically backed up by Learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,6 +1,3 @@
 function hideWhenClicked(){
-  
-If you want to close it you can either hide it or remove it from the page. To hide it you would do some javascript like:
-
 this.parentNode.style.display = 'none';
 }
